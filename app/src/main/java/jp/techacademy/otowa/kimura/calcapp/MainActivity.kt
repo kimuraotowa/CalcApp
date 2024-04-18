e384c39d5b6bdb77135ac6a0d799ad314833047c
ユーザー名 <o-kimura@i-enter.co.jp>
2024-04-18 11:21:32 +0900

    Lesson4 Chapter5.3再提出

--- app/src/main/java/jp/techacademy/otowa/kimura/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/otowa/kimura/calcapp/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            //レイアウトxmlで定義されたEditText,Button検索
+        //レイアウトxmlで定義されたEditText,Button検索
         val et1 = findViewById<EditText>(R.id.et1)
         val et2 = findViewById<EditText>(R.id.et2)
         val AddButton = findViewById<Button>(R.id.btn3)
@@ -26,35 +27,78 @@ class MainActivity : AppCompatActivity() {
         AddButton.setOnClickListener {
             val num1 = et1.text.toString().toDoubleOrNull() ?: 0.0
             val num2 = et2.text.toString().toDoubleOrNull() ?: 0.0
-            val result = num1 + num2
-            openResultActivity(result)
+
+            //num1とnum2がどちらでもないかつnum2は0.0でもない場合①動く
+            if (num1 != null && num2 != null && num2 != 0.0) {
+                //①
+                val result = num1 / num2
+                openResultActivity(result)
+            //num1またはnum2がnullの場合またnum2が0の場合エラーメッセージ表示
+            } else {
+                showErrorMessage()
+            }
         }
 
         mulButton.setOnClickListener {
             val num1 = et1.text.toString().toDoubleOrNull() ?: 0.0
             val num2 = et2.text.toString().toDoubleOrNull() ?: 0.0
             val result = num1 * num2
-            openResultActivity(result)
+
+            //num1とnum2がどちらでもないかつnum2は0.0でもない場合
+            if (num1 != null && num2 != null && num2 != 0.0) {
+                val result = num1 / num2
+                openResultActivity(result)
+            //num1またはnum2がnullの場合またnum2が0の場合エラーメッセージ表示
+            } else {
+                showErrorMessage()
+            }
         }
 
         DivButton.setOnClickListener {
             val num1 = et1.text.toString().toDoubleOrNull() ?: 0.0
             val num2 = et2.text.toString().toDoubleOrNull() ?: 0.0
-            val result = num1 / num2
-            openResultActivity(result)
+
+            //num1とnum2がどちらでもないかつnum2は0.0でもない場合
+            if (num1 != null && num2 != null && num2 != 0.0) {
+                val result = num1 / num2
+                openResultActivity(result)
+            //num1またはnum2がnullの場合またnum2が0の場合エラーメッセージ表示
+            } else {
+                showErrorMessage()
+            }
         }
 
         SubButton.setOnClickListener {
             val num1 = et1.text.toString().toDoubleOrNull() ?: 0.0
             val num2 = et2.text.toString().toDoubleOrNull() ?: 0.0
             val result = num1 - num2
-            openResultActivity(result)
+
+            //num1とnum2がどちらでもないかつnum2は0.0でもない場合
+            if (num1 != null && num2 != null && num2 != 0.0) {
+                val result = num1 / num2
+                openResultActivity(result)
+            //num1またはnum2がnullの場合またnum2が0の場合エラーメッセージ表示
+            } else {
+                showErrorMessage()
+            }
         }
     }
+
     //openResultActivity　→　計算結果をSecondActivityに送信するメソッド
     private fun openResultActivity(result: Double) {
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra("RESULT", result)
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            putExtra("RESULT", result)
+        }
         startActivity(intent)
+    }
+
+    //showErrorMessageプライベート関数
+    private fun showErrorMessage() {
+        //ダイアログ作成
+        AlertDialog.Builder(this)
+            .setTitle("エラー")
+            .setMessage("数値を入力してください")
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
